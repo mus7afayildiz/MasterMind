e73adb6e433d1b623a18f0a834d74b1340c9716b
Mustafa Yildiz <pj28jjj@eduvaud.ch>
2023-09-15 15:47:27 +0200

    Des messages de bienvenue et d'information ont été créés.

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -10,7 +10,17 @@ namespace MasterMind
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hllro");
+
+            Console.WriteLine("Bienvenue dans le jeu MasterMind");
+            Console.WriteLine("Il s'agit d'un jeu de devinettes de couleurs.");
+            Console.WriteLine("Si vous devinez respectivement 4 de ces 7 couleurs, vous gagnez la partie. Vous avez 10 suppositions.");
+            Console.WriteLine("Comme couleurs, utilisez les lettres « G » pour Gris, « Y » pour Jaune, « W » pour Blanc, « R » pour Rouge, « B » pour Bleu, « M » pour Magenta et « C » pour Cyan.");
+            Console.ReadLine();
+
+
+
+
+           
         }
     }
 }
