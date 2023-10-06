8a23ffb1f3fbe3cc283d5a2a9feac2176e9c6f80
Mustafa Yildiz <pj28jjj@eduvaud.ch>
2023-10-06 15:58:55 +0200

    ecrir des boucles et evaluer les couleurs

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -10,21 +10,23 @@ namespace MasterMind
     {
         static void Main(string[] args)
         {
+            //
             Console.WriteLine("Bienvenue dans le jeu MasterMind");
             Console.WriteLine("Il s'agit d'un jeu de devinettes de couleurs.");
             Console.WriteLine("Si vous devinez respectivement 4 de ces 7 couleurs, vous gagnez la partie. Vous avez 10 suppositions.");
             Console.WriteLine("Comme couleurs, utilisez les lettres « G » pour Gris, « Y » pour Jaune, « W » pour Blanc, « R » pour Rouge, « B » pour Bleu, « M » pour Magenta et « C » pour Cyan.");
-            Console.ReadLine();
+            //Console.ReadLine();
 
             //Couleurs pouvant être choisies
             char[] couleurs = { 'G', 'Y', 'W', 'R', 'B', 'M', 'C'};
             char[] couleursParOrdinateur = new char[4];
             bool gegnerJeu = false;
             string couleursParUtilisateur = "";
-            int compteurIndex=0;
+            int compteurBienPlace=0;
+            int compteurMalPlace = 0;
             int compteurCouleurs=0;
             int compteurTour = 0;
-            int compteurOpposé = 4;
+            //int compteurOpposé = 4;
 
             //L'ordinateur génère 4 nombres aléatoires compris entre 0 et 7.
             //Il utilise les nombres générés comme index. Ajoute les couleurs de cet index au nouveau array.
@@ -40,48 +42,90 @@ namespace MasterMind
             }
 
             Console.WriteLine(couleursParOrdinateur);
-            Console.ReadLine();
+            //Console.ReadLine();
 
             while (!gegnerJeu && compteurTour < 10)
             {           
-                //L'utilisateur est invité à choisir 4 couleurs. Les couleurs saisies sont transférées vers une variable.
+                //L'utilisateur est invité à choisir 4 couleurs. Les couleurs saisies sont transférées vers une variable et transformer uppercase.
                 Console.WriteLine("Entrez 4 couleurs pour deviner.");
-                couleursParUtilisateur = Console.ReadLine();
+                couleursParUtilisateur = Console.ReadLine().ToUpper();
 
-                if (couleursParUtilisateur.Length !=4)
+                if (couleursParUtilisateur.Length != 4)
                 {
                     Console.WriteLine("Vous devez choisir 4 lettres pour couleurs");
                     continue;
                 }
 
+
+                /*
+                for (int i = 0; i < 4; i++) {
+                    if (couleursParUtilisateur[i] == couleursParOrdinateur[i])
+                    {
+                        Console.WriteLine("juste");
+                        compteurCouleurs++;
+                        compteurBienPlace++;
+                    }
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if(couleursParUtilisateur[i] == couleursParOrdinateur[j] && couleursParUtilisateur[i] == couleursParOrdinateur[i])
+                    {
+                        compteurMalPlace++;
+
+
+                    }
+                    }
+                    
+
+                    
+                
+                }*/
+
+                
                 //Les couleurs de l'ordinateur et de l'utilisateur sont comparées.
                 for (int i = 0; i < 4; i++) {
                     for (int j = 0; j < 4; j++) {
-                        Console.WriteLine("i : "+ couleursParOrdinateur[i]);
-                        Console.WriteLine("j : " + couleursParUtilisateur[j]);
-                        Console.ReadLine();
+                       // Console.WriteLine("i : "+ couleursParOrdinateur[i]);
+                        //Console.WriteLine("j : " + couleursParUtilisateur[j]);
+                       // Console.ReadLine();
                         if (couleursParOrdinateur[i] == couleursParUtilisateur[j])
                         {
                             if (i == j)
                             {
-                                compteurIndex++;
-                                Console.WriteLine("Yer ve renk Ayni" + compteurIndex);
-                                Console.ReadLine();
+                                compteurBienPlace++;
+                               // Console.WriteLine("la même place et la même couleur" + compteurBienPlace);
+                               // Console.ReadLine();
                             }
-
+                            else {
+                                
+                                compteurMalPlace++;
+                                break;
+                               // Console.WriteLine("la même couleur mais mal places" + compteurMalPlace);
+                               // Console.ReadLine();
+                            }
                             compteurCouleurs++;
-                            Console.WriteLine("Couleurs" + compteurCouleurs);
-                            Console.ReadLine();
+                           // Console.WriteLine("Couleurs" + compteurCouleurs);
+                           // Console.ReadLine();
                         }
                                      
                     }
                 }
-                Console.WriteLine("siz " + (compteurOpposé - compteurIndex) + " kadar yeri tahmin edemediniz");
-                Console.WriteLine("siz "+ compteurIndex + " kadar yeri dogru tahmin ettiniz");
-                Console.WriteLine("siz "+ compteurCouleurs + " kadar rengi dogru tahmin ettiniz");
+                
+                Console.WriteLine("Vous trouvez " + compteurBienPlace + " places vrai");
+                Console.WriteLine("Vouz trouvez " + compteurCouleurs + " couleurs vrai");
+                Console.WriteLine("Vouz trouvez " + compteurMalPlace + " couleurs vrai mais mal places" );
+                
                 Console.ReadLine();
                 compteurTour++;
-                Console.WriteLine(compteurTour);
+                Console.WriteLine(10-compteurTour + " fois vous pouvez essaier");
+                Console.ReadLine();
+            }
+            if (compteurTour < 10 && gegnerJeu == true)
+            {
+                Console.WriteLine("Felicitation vous gagnez :)");
+                Console.ReadLine();
+            }
+            else {
+                Console.WriteLine("Désole vous avez perdu :(");
                 Console.ReadLine();
             }
         }
