9e0a9a4c150c4e4e9aff215ecfd7bf321511c34f
mus7afayildiz <mus7afayildiz@yandex.com>
2023-10-23 03:26:47 +0200

    erreur corrigée

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -9,34 +9,39 @@ namespace MasterMind
     internal class Program
     {
         static void Main(string[] args)
-        {         
+        {
             //Couleurs pouvant être choisies.
-            char[] couleurs = { 'G', 'Y', 'W', 'R', 'B', 'M', 'C'};
+            char[] couleurs = { 'G', 'Y', 'W', 'R', 'B', 'M', 'C' };
             char[] couleursParOrdinateur = new char[4];
             bool gegnerJeu = false;
             string couleursParUtilisateur = " ";
             int compteurBienPlace = 0;
             int compteurMalPlace = 0;
-            int compteurTour = 1;         
+            int compteurTour = 1;
 
             char[] couleursParUtilisateurManipule = new char[4];
             char[] couleursParOrdinateurManipule = new char[4];
 
             introduction();
             genereCouleurs();
-          
-            //couleursParOrdinateur = "CCBC".ToArray();
-            //Console.WriteLine(couleursParOrdinateur);      
-            //Console.ReadLine();
+            
 
+            //couleursParOrdinateur = "BBGB".ToArray();
+            Console.WriteLine("ordina");
+            Console.WriteLine(couleursParOrdinateur);
+            Console.WriteLine(couleursParOrdinateurManipule);
+            Console.WriteLine("Mani");
+            //Console.ReadLine();
+            
             //Le jeu continue jusqu'à ce que vous gagniez ou perdiez.
             while (!gegnerJeu && compteurTour <= 10)
             {
-                demandeCouleurs();               
+                demandeCouleurs();
+                //Comparer(couleursParOrdinateurManipule, couleursParUtilisateurManipule);
                 compareMemeIndex();
                 compareDifferentIndex();
                 montreResultat();
-              
+
                 if (compteurTour < 10 && compteurBienPlace == 4)
                 {
                     Console.WriteLine("Félicitations, Vous avez Gagné. :) ");
@@ -44,17 +49,17 @@ namespace MasterMind
                     gegnerJeu = true;
                     break;
                 }
-                else if(compteurTour==10)
+                else if (compteurTour == 10)
                 {
                     Console.WriteLine("Désole, Vous avez Perdu :( ");
-                    Console.ReadLine();                 
+                    Console.ReadLine();
                     break;
                 }
 
                 compteurBienPlace = 0;
                 compteurMalPlace = 0;
                 compteurTour++;
-                Console.WriteLine("Encore " + (11 - compteurTour) + " fois vous pouvez essaier. \n");             
+                Console.WriteLine("Encore " + (11 - compteurTour) + " fois vous pouvez essaier. \n");
             }
 
             //introduction de jeu.
@@ -64,7 +69,7 @@ namespace MasterMind
                 Console.WriteLine("Il s'agit d'un jeu de devinettes de couleurs.");
                 Console.WriteLine("Si vous devinez respectivement 4 de ces 7 couleurs, vous gagnez la partie. Vous avez 10 suppositions.\n");
                 Console.WriteLine("Comme couleurs, utilisez les lettres « G » pour Gris, « Y » pour Jaune, « W » pour Blanc, « R » pour Rouge, « B » pour Bleu, « M » pour Magenta et « C » pour Cyan.");
-                Console.WriteLine("GYWRBMC \n");             
+                Console.WriteLine("GYWRBMC \n");
             }
 
             //L'ordinateur génère 4 nombres aléatoires compris entre 0 et 7.
@@ -82,10 +87,11 @@ namespace MasterMind
 
             //L'utilisateur est invité à choisir 4 couleurs. Les couleurs saisies sont transférées vers une variable et transformer uppercase et char.
             void demandeCouleurs()
-            {  
+            {
                 Console.WriteLine("Entrez 4 couleurs pour deviner.");
-                couleursParUtilisateur = Console.ReadLine().ToUpper() ;
+                couleursParUtilisateur = Console.ReadLine().ToUpper();
                 couleursParUtilisateurManipule = couleursParUtilisateur.ToCharArray();
+                couleursParOrdinateurManipule = (char[])couleursParOrdinateur.Clone();
                 Console.WriteLine(couleursParUtilisateurManipule);
                 verifieDonne();
             }
@@ -94,13 +100,15 @@ namespace MasterMind
             void verifieDonne()
             {
                 if (couleursParUtilisateur.Length != 4)
-                {                   
+                {
                     Console.WriteLine("Vous devez choisir 4 lettres pour couleurs. \n");
                     demandeCouleurs();
-                } else if (couleursParUtilisateur.Length == 4) {
+                }
+                else if (couleursParUtilisateur.Length == 4)
+                {
                     for (int i = 0; i < couleurs.Length; i++)
                     {
-                        for (int j = 0; j < couleursParUtilisateurManipule.Length; j++)
+                        for (int j = 0; j < couleursParUtilisateur.Length; j++)
                         {
                             if (!couleurs.Contains(couleursParUtilisateur[j]))
                             {
@@ -112,22 +120,80 @@ namespace MasterMind
                     }
                 }
             }
+            /*
+            void Comparer(char[] ord, char[] uti) {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if ((i == j) && (ord[i] == uti[j]))
+                        {
+                            ord[i] = '0';
+                            uti[j] = '1';
+                            Console.WriteLine(couleursParOrdinateur);
+                            Console.Write("ord :");
+                            Console.WriteLine(ord);
+                           
+                            Console.WriteLine(couleursParUtilisateur);
+                            Console.Write("ut :");                          
+                            Console.WriteLine(uti);
+                            compteurBienPlace++;
+                            break;
+                        }
+                    }
+                }
+            
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if ((i != j) && (ord[i] == uti[j]))
+                        {
+                            ord[i] = 'X';
+                            uti[j] = 'Z';
+                            Console.WriteLine(couleursParOrdinateur);
+                            Console.Write("orddif :");                          
+                            Console.WriteLine(ord);
+                            Console.WriteLine("/n");
+                            Console.WriteLine(couleursParUtilisateur);                           
+                            Console.Write("utdif :");                        
+                            Console.WriteLine(uti);
+                            compteurMalPlace++;
+                            break;
+                        }
+                    }
+                }
 
+
+          
+
+            }
+
+  */
+
+            
             //Les couleurs de l'ordinateur et de l'utilisateur sont comparées par même index.
             void compareMemeIndex()
-            { 
+            {
                 for (int i = 0; i < 4; i++)
                 {
                     for (int j = 0; j < 4; j++)
                     {
                         if ((i == j) && (couleursParOrdinateur[i] == couleursParUtilisateurManipule[j]))
                         {
-                            couleursParUtilisateurManipule[j] = 'X';
-                            couleursParOrdinateurManipule[i] = 'Y';
+                            couleursParOrdinateurManipule[i] = '0';
+                            couleursParUtilisateurManipule[j] = '1';
+                            Console.WriteLine(" ");
+                            Console.Write("ord :");
+                            Console.WriteLine(couleursParOrdinateur);
+                            Console.WriteLine(ordCol);
+                            Console.WriteLine(couleursParOrdinateurManipule);
+                            Console.Write("ut :");
+                            Console.WriteLine(couleursParUtilisateur);
                             Console.WriteLine(couleursParUtilisateurManipule);
-                            compteurBienPlace++;                           
+                            compteurBienPlace++;
                             break;
-                        }                       
+                        }
                     }
                 }
             }
@@ -141,22 +207,34 @@ namespace MasterMind
                     {
                         if ((i != j) && (couleursParOrdinateurManipule[i] == couleursParUtilisateurManipule[j]))
                         {
-                            couleursParUtilisateurManipule[j] = 'X';
-                            Console.WriteLine(couleursParUtilisateur[j]);
-                            compteurMalPlace++;                          
+                            couleursParOrdinateurManipule[i] = 'X';
+                            couleursParUtilisateurManipule[j] = 'Z';
+                            Console.WriteLine(couleursParUtilisateurManipule[j]);
+                            Console.Write("orddif :");
+                            Console.WriteLine(couleursParOrdinateur);
+                            Console.WriteLine(couleursParOrdinateur);
+                            Console.WriteLine(couleursParOrdinateurManipule);
+                            Console.Write("utdif :");
+                            Console.WriteLine(couleursParUtilisateur);
+                            Console.WriteLine(couleursParUtilisateurManipule);
+                            compteurMalPlace++;
                             break;
                         }
                     }
                 }
             }
+            
 
             //Après évaluation, le résultat est affiché sur la console.
             void montreResultat()
             {
+                Console.WriteLine(" ");
                 Console.WriteLine(couleursParOrdinateur);
+                Console.Write("Les Couleurs Possibles: ");
+                Console.WriteLine(couleurs);
                 Console.WriteLine("Essai " + compteurTour + " " + couleursParUtilisateur);
                 Console.WriteLine("=> Ok: " + compteurBienPlace);
-                Console.WriteLine("=> Mauvaise Position : " + compteurMalPlace + "\n");              
+                Console.WriteLine("=> Mauvaise Position : " + compteurMalPlace + "\n");
             }
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # MasterMind
-Projet MasterMind
+
+
+MasterMind - Règles du jeu.
+
+-L'ordinateur choisit une séquence de couleurs. Le nombre de couleurs correspond à la longueur du code. La longueur du code par défaut est de 4.
+
+-L'objectif du jeu est de deviner les positions exactes des couleurs dans la séquence de l'ordinateur.
+
+-Après avoir rempli une ligne avec vos suppositions et cliqué sur le bouton "Enter", l'ordinateur répond avec le résultat de votre supposition.
+
+-Chaque couleur que vous estimez est correcte si elle correspond à la couleur et à la position correctes dans la séquence de codes.
+
+-Même si chaque couleur de votre prédiction est la bonne couleur dans la séquence de codes, si elle n'est pas dans la bonne position, elle s'affichera comme étant la mauvaise position.
+
+-Vous gagnez le jeu lorsque vous parvenez à deviner toutes les couleurs de la séquence de code et qu'elles sont toutes à la bonne position.
+
+-Vous perdez le jeu si vous utilisez toutes les tentatives sans deviner la séquence de code de l'ordinateur.
+
+
+Comment jouer à ce jeu:
+
+Après avoir rempli la ligne en choisissant 4 couleurs sur 7, vous pouvez modifier votre sélection avant de demander à l'ordinateur de répondre à votre supposition. Une fois que vous êtes satisfait de votre réponse, cliquez sur le bouton « Entrée » et obtenez la réponse de l'ordinateur.
+
+Bonne chance !
